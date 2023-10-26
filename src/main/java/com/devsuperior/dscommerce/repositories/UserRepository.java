1a77e2994d7d46f78dbfadcfdee617e55daccf53
PedroZangirolami <pedrozangirolami@gmail.com>
2023-10-26 10:46:31 -0300

    get user loged

--- src/main/java/com/devsuperior/dscommerce/repositories/UserRepository.java
+++ src/main/java/com/devsuperior/dscommerce/repositories/UserRepository.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     @Query(nativeQuery = true, value = """
@@ -20,4 +21,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
 			""")
     List<UserDetailsProjection> searchUserAndRolesByEmail(String email);
 
+	Optional<User> findByEmail(String email);
 }
